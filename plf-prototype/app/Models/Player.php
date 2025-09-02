577a344897daf88c41e7baae4240b899fe86caa2
JkShadman <158991854+JkShadman@users.noreply.github.com>
2025-09-02 20:39:48 +0600

    Update Player.php

--- plf-prototype/app/Models/Player.php
+++ plf-prototype/app/Models/Player.php
@@ -1,1 +1,10 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 
+class Player extends Model {
+  protected $fillable=['first_name','second_name','name','position','club_id','price','is_injured','injury_level','minutes'];
+  public function club(){ return $this->belongsTo(Club::class); }
+  public function stats(){ return $this->hasMany(PlayerStats::class); }
+  public function injuries(){ return $this->hasMany(Injury::class); }
+}
